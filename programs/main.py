58d460f774acd066118644a09f9e862204225cdd
Dexter <K.W.Yilmaz@gmail.com>
2022-05-09 16:39:17 +0200

    init

--- programs/main.py
+++ programs/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response, HTTPException
 from sqlalchemy.orm import Session
 from typing import Optional
 import schemas, models
@@ -22,14 +22,12 @@ def index():
 
 
 @app.get("/program")
-def program(limit=10, published: bool = True, sort: Optional[str] = None):
-    if published:
-        return {"data": f"{limit} published blogs from the list"}
-    else:
-        return {"data": f"{limit} blogs from the list"}
+def all(db: Session = Depends(get_db)):
+    programs = db.query(models.Program).all()
+    return programs
 
 
-@app.post("/program")
+@app.post("/program", status_code=201)
 def create(request: schemas.Program, db: Session = Depends(get_db)):
     new_program = models.Program(title=request.title, body=request.body)
     db.add(new_program)
@@ -38,6 +36,19 @@ def create(request: schemas.Program, db: Session = Depends(get_db)):
     return new_program
 
 
+@app.delete("/program/{id}", status_code=200)
+def delete(id: int, db: Session = Depends(get_db)):
+    program = db.query(models.Program).filter(models.Program.id == id).first()
+    if not program:
+        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not available")
+    db.query(models.Program).filter(models.Program.id == id).delete(synchronize_session=False)
+    db.commit()
+    return f'Deleted program with id {id}'
+    
+
 @app.get("/program/{id}")
-def get_program(id: int):
-    return {"data": id}
+def single(id: int, response: Response, db: Session = Depends(get_db)):
+    program = db.query(models.Program).filter(models.Program.id == id).first()
+    if not program:
+        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not available")
+    return program
