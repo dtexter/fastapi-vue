3acb9f090c002384fb25bcd1d4b27e8bef5ef12e
Dexter <K.W.Yilmaz@gmail.com>
2022-05-10 02:28:40 +0200

    init

--- programs/main.py
+++ programs/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Depends, Response, HTTPException
 from sqlalchemy.orm import Session
-from typing import Optional
-import schemas, models
+from typing import Optional, List
+import schemas, models, hashing
 from database import engine, SessionLocal
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -16,39 +17,62 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def index():
-    return {"data": {"name": "Dexter"}}
 
-
-@app.get("/program")
+@app.get("/program", response_model=List[schemas.ShowProgram], tags=["programs"])
 def all(db: Session = Depends(get_db)):
     programs = db.query(models.Program).all()
     return programs
 
 
-@app.post("/program", status_code=201)
+@app.post("/program", status_code=201, response_model=schemas.Program, tags=["programs"])
 def create(request: schemas.Program, db: Session = Depends(get_db)):
-    new_program = models.Program(title=request.title, body=request.body)
+    new_program = models.Program(title=request.title, body=request.body, user_id=1)
     db.add(new_program)
     db.commit()
     db.refresh(new_program)
     return new_program
 
 
-@app.delete("/program/{id}", status_code=200)
-def delete(id: int, db: Session = Depends(get_db)):
+@app.get("/program/{id}", status_code=200, response_model=schemas.ShowProgram, tags=["programs"])
+def show(id: int, response: Response, db: Session = Depends(get_db)):
     program = db.query(models.Program).filter(models.Program.id == id).first()
     if not program:
-        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not available")
-    db.query(models.Program).filter(models.Program.id == id).delete(synchronize_session=False)
-    db.commit()
-    return f'Deleted program with id {id}'
-    
-
-@app.get("/program/{id}")
-def single(id: int, response: Response, db: Session = Depends(get_db)):
-    program = db.query(models.Program).filter(models.Program.id == id).first()
-    if not program:
-        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not available")
+        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not found")
     return program
+
+
+@app.put("/program/{id}", status_code=202, tags=["programs"])
+def update(id: int, request: schemas.Program, db: Session = Depends(get_db)):
+    program = db.query(models.Program).filter(models.Program.id == id)
+    if not program.first():
+        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not found")
+    program.update(request.dict())
+    db.commit()
+    return f"Updated program with id {id}"
+
+
+@app.delete("/program/{id}", status_code=200, tags=["programs"])
+def delete(id: int, db: Session = Depends(get_db)):
+    program = db.query(models.Program).filter(models.Program.id == id)
+    if not program.first():
+        raise HTTPException(status_code=404, detail=f"Program with the id {id} is not found")
+    program.delete(synchronize_session=False)
+    db.commit()
+    return f"Deleted program with id {id}"
+
+
+@app.post("/user", status_code=201, response_model=schemas.ShowUser, tags=["users"])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/user/{id}", status_code=200, response_model=schemas.ShowUserPrograms, tags=["users"])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User with the id {id} is not found")
+    return user
--- programs/models.py
+++ programs/models.py
@@ -1,8 +1,22 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from database import Base
 
+
 class Program(Base):
-    __tablename__ = 'programs'
+    __tablename__ = "programs"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    creator = relationship("User", back_populates="programs")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    programs = relationship("Program", back_populates="creator")
